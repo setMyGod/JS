3779d268fe6ae7940592af839fa64307f8a5e573
Ivan Hrytsyna <ivangritsina@gmail.com>
2021-11-30 11:25:24 +0200

    классная работа номер 7

--- classwork7/classwork7.js
+++ classwork7/classwork7.js
@@ -7,20 +7,41 @@
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 
-function CarsS (model, manufacturer, year, maxspeed, volume){
-    this.model = model;
-    this.manufacturer = manufacturer;
-    this.year = year;
-    this.maxspeed = maxspeed;
-    this.volume = volume;
+function CarsS (modelx, manufacturerx, yearx, maxspeedx, volumex){
+    this.modelx = modelx
+    this.manufacturerx = manufacturerx
+    this.yearx = yearx
+    this.maxspeedx = maxspeedx
+    this.voumex = volumex
+    this.drivers = function (){
+        console.log(`їдемо зі швидкістю ${maxspeedx} на годину`);
+    }
+    this.infos = function (){
+        console.log(this.modelx, this.manufacturerx, this.yearx, this.maxspeedx, this.voumex);
+    }
+    this.superspeed = function (upspeed){
+        console.log(this.maxspeedx = +upspeed);
+    }
+    this.newyears = function (supeyears){
+        console.log(this.yearx = supeyears);
+    }
+    this.newdrivers = function (drivers){
+        console.log(this.drivers = drivers);
+
+    }
 
 
 
 
 
-
-
-console.log(new CarsS('X5', 'bmw', 2020, 280, 3));
+}
+let c = new CarsS('X5', 'BMW', 2020, 300, 3.5);
+c.drivers()
+c.superspeed(380)
+c.newyears(2021)
+c.newdrivers("Vasya ROgov")
+c.infos()
+console.log(c);
 
 
 class Car {
@@ -51,9 +72,48 @@ class Car {
 }
 let car = new Car(2109,'Vaz',1998,200,1.4)
 car.greeting()
-
 car.increaseMaxSpeed(50)
 car.cangeYear(2001)
 car.addDriver("Petro")
 car.info()
 console.log(car);
+
+
+
+// -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
+// Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+//     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+//     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку*/
+
+class girl {
+    constructor(name, age, size) {
+        this.name = name;
+        this.age = age;
+        this.size = size;
+    }
+
+
+}
+    let girls =[
+        new girl('girl1',12,23),
+        new girl('girl2',32,32),
+        new girl('girl3',16,20),
+        new girl('girl4',15,42),
+        new girl('girl5',14,36),
+        new girl('girl6',53,34),
+        new girl('girl7',23,35),
+        new girl('girl8',83,31),
+        new girl('girl9',15,29),
+        new girl('girl 10',53,30),
+    ]
+class Prince {
+    constructor(name, age, shoe) {
+        this.name = name;
+        this.age = age;
+        this.shoe = shoe;
+    }
+
+
+}
+let prince = new Prince('prince1',33,35)
+console.log(girls.find(value => value.size === prince.shoe));
