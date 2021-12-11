206caf60ffd41537d449b37e263a346bd379d9b0
SetMyGod1488 <ivangritsina@gmail.com>
2021-12-11 15:22:59 +0200

    домашняя работа 11

--- homework11/homework11.js
+++ homework11/homework11.js
@@ -1,57 +1,41 @@
 // -створити форму з інпутами для name та age.
 //     При відправці форми записати об'єкт в localstorage
-let div = document.createElement('div');
 let form = document.createElement('form');
-let inputname = document.createElement('input');
+let name = document.createElement('input');
+let age = document.createElement('input');
+let btn = document.createElement('button');
+btn.innerText = 'Create object'
+age.setAttribute('type', 'number');
+name.setAttribute('type', 'string');
+document.body.append(form, btn);
+form.append(name, age);
+btn.onclick = function () {
+    localStorage.setItem('mashine', JSON.stringify({MODEL:name.value, YEARS_OLD:age.value}))
+};
 
-let inputage = document.createElement('input');
-let button = document.createElement('button');
-form.append(inputname, inputage)
-document.body.append(div,button)
-div.append(form)
-div.style.width = '100px'
-button.innerHTML = 'Get it'
-button.addEventListener('click',function (){
-    localStorage.setItem('name',JSON.stringify(inputname.value))
-    localStorage.setItem('age',JSON.stringify(inputage.value))
 
-})
 
 // -створити форму з інпутами для model,type та volume автівки.
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
-let cardiv = document.createElement('div');
-let carform = document.createElement('form');
-let modelinput = document.createElement('input');
-let typeinput = document.createElement('input');
-let volumeinput = document.createElement('input');
-let carbutton = document.createElement('button');
-carbutton.innerHTML = 'push form'
-carform.append(modelinput,typeinput,volumeinput)
-document.body.append(carbutton,carform, cardiv)
-cardiv.append(carform,carbutton)
-cardiv.style.marginTop = '30px'
-carform.style.width = '100px'
 
 
 
+let forma = document.createElement('form');
+let model = document.createElement('input');
+let type = document.createElement('input');
+let volume = document.createElement('input');
+let button = document.createElement('button');
+button.innerText = 'Save'
+forma.append(model, type, volume);
+document.body.append(forma,button)
+button.onclick = function () {
+    let mass = []
+
+    mass.push(model.value,type.value,volume.value)
+    console.log(mass);
+
+    localStorage.setItem('cars',JSON.stringify(mass))
 
 
-carbutton.addEventListener('click',function (){
+};
 
-
-    localStorage.setItem('model',JSON.stringify(modelinput.value))
-    localStorage.setItem('type',JSON.stringify(typeinput.value))
-    localStorage.setItem('volume', JSON.stringify(volumeinput.value))
-
-
-    let mJSON = localStorage.getItem('model');
-    let tJSON = localStorage.getItem('type');
-    let vJSON = localStorage.getItem('volume');
-    console.log(JSON.parse(mJSON));
-    console.log(JSON.parse(tJSON));
-    console.log(JSON.parse(vJSON));
-    let mass =[mJSON,tJSON,vJSON]
-    localStorage.setItem('array',mass)
-
-
-})
