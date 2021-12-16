44c8e97d062e7790817b109102709d82d9a9c553
Ivan Hrytsyna <ivangritsina@gmail.com>
2021-12-16 17:20:39 +0200

    минипроект

---
+++ miniproject/miniproject.js
@@ -0,0 +1,54 @@
+// В index.html
+// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
+// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
+//
+//
+// На странице user-details.html:
+// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
+//
+// На странице post-details.html:
+// 7 Вывести всю, без исключения, информацию про объект post на кнопку/ссылку которого был совершен клик ранее.
+// 8 Ниже информации про пост, вывести все комментарии текущего поста (эндпоинт для получения информации - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+//
+// Стилизация проекта -
+// index.html - все блоки с user - по 2 в ряд. кнопки/ссылки находяться под информацией про user.
+// user-details.html - блок с информацией про user вверху страницы. Кнопка ниже, на 90% ширины страницы, по центру.
+// блоки с краткой информацией про post - в ряд по 5 объектов.
+// post-details.html - блок с информацией про пост вверху. Комментарии - по 4 в ряд.
+// Все без исключения элементы, который характеризируют user,post,comment  визуализировать, так, что бы было видно их блоки (дать задний фон + margin. Иными словами - крайне четкая сетка)
+//
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        for( let i of users){
+            let div = document.createElement('div');
+            document.body.append(div)
+            let p = document.createElement("p");
+            let h2 = document.createElement("h2");
+            div.append(h2)
+            div.append(p)
+            p.innerHTML = `id: ${i.id}`
+            h2.innerHTML = `name: ${i.name}`
+            let button = document.createElement('button');
+            div.append(button)
+            let a = document.createElement('a');
+            div.append(a)
+            a.append(button)
+            a.href = 'post-details.html'
+            button.innerHTML = `Favorite user`
+
+            button.onclick = function (e){
+
+
+            }
+
+
+
+        }
+    })
+
