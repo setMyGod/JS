12c3b1ea4094f0d456c1ef6b76ae11ccc65ffa12
SetMyGod1488 <ivangritsina@gmail.com>
2021-12-19 15:36:06 +0200

    Дз14 Ассинхронные функции 1.Промисы 2.Колбеки 3.Ассинк

--- Homework14/homework14.js
+++ Homework14/homework14.js
@@ -1,9 +1,9 @@
 function wakeup (isWorkingDay = true){
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(()=>{
-        if(isWorkingDay){
-            resolve('просыпаюсь')
-        }
+            if(isWorkingDay){
+                resolve('просыпаюсь',)
+            }
 
         },1000)
 
@@ -11,16 +11,16 @@ function wakeup (isWorkingDay = true){
 }
 function goEat(){
     return new Promise((resolve) => {
-       setTimeout(()=>{
+        setTimeout(()=>{
 
-               resolve('Кушаю')
+            resolve('Кушаю')
 
 
-       },1000)
+        },1000)
     });
 }
 function goWork(){
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(()=>{
             resolve('иду на работу')
         },1000)
@@ -33,7 +33,7 @@ function doneMyjob() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve('Работаю')
-        }, 1000)
+        },1000)
 
 
     })
@@ -43,29 +43,85 @@ function backtoHome (){
     return new Promise(resolve => {
         setTimeout(()=>{
             resolve('Возвращаюсь домой')
-        }, 1000)
+        },1000)
     })
 }
 
 wakeup(true)
-.then(morning =>{
-    console.log(morning);
-    return goEat()
-})
-.then(eat =>{
-    console.log(eat);
-    return goWork()
+    .then(morning =>{
+        console.log(morning, 'Promise function');
+        return goEat()
+    })
+    .then(eat =>{
+        console.log(eat, 'Promise function');
+        return goWork()
+
+    })
+    .then(workingday =>{
+        console.log(workingday, 'Promise function')
+        return doneMyjob()
+    })
+
+    .then(donemywork =>{
+        console.log(donemywork, 'Promise function');
+        return backtoHome()
+    })
+    .then(homeBack=>{
+        console.log(homeBack, 'Promise function');
+    })
+
+function goodmorning (up, callback){
+    setTimeout(()=>{
+        if(up){
+            console.log('7:30 Подъем', '-CallBAck')
+            callback()
+        }
+        else {
+            console.log('Zzz-zzz');
+        }
+    },7000)
+
+}
+goodmorning(true,()=>{
+    console.log('8:00 Завтрак','-CallBAck')
+
 
 })
-.then(workingday =>{
-    console.log(workingday)
-    return doneMyjob()
+
+function rabota (workday, finish){
+    setTimeout(()=>{
+        if(workday){
+            console.log('Работаю на работе','-CallBAck')
+            finish()
+        }
+        else {
+            console.log('ушел с работы');
+        }
+    },8000)
+}
+rabota(true,()=>{
+    fly(true)
+
 })
 
-.then(donemywork =>{
-    console.log(donemywork);
-    return backtoHome()
-})
-.then(homeBack=>{
-    console.log(homeBack);
-})
+function fly(egipet){
+    if(egipet){
+
+        console.log('засунул синхронную функцию в коллбек, полетел в Египет','-CallBAck');
+
+    }
+}
+
+
+
+
+setTimeout(()=>{
+    async function teach(){
+        const vozvratdomoi = await backtoHome(true);
+        const kushat = await goEat(vozvratdomoi);
+        console.log(vozvratdomoi, '-=Async function=-')
+        console.log(kushat, '-=Async function=-');
+    }
+    teach()
+},10000)
+
